43c0bb86bbdb61ac0a4d84f60f307dffb26a5a15
Lancer385 <102915355+Lancer385@users.noreply.github.com>
2023-10-31 18:59:55 +0200

    add a way to calculate using buttons

--- script.js
+++ script.js
@@ -27,3 +27,59 @@ function operate(num1, operator, num2){
 }
 
 
+let calculation = {
+    num1 : 0,
+    operator: "",
+    num2 : 0,
+};
+const buttons = {
+    backSpace : document.querySelector("#backSpace"),
+    equal : document.querySelector("#equal"),
+    numbers : document.querySelectorAll(".numbers"),
+    operators : document.querySelectorAll(".operator")
+};
+const display = {
+    input : document.querySelector("#input"),
+};
+let deletion = 0;
+
+
+buttons.numbers.forEach((number)=> {
+    number.addEventListener("click", ()=>{
+        display.input.innerText += number.innerText;
+        if (calculation.num2 == 0){
+            calculation.num1 = parseInt(display.input.innerText);
+        }
+        else {
+            calculation.num2 = parseInt(display.input.innerText);
+        }
+        console.log("num1:" ,calculation.num1);
+        console.log("num2:", calculation.num2);
+    })
+})
+
+
+buttons.operators.forEach((operator) =>{
+    operator.addEventListener("click", () =>{
+        switch (operator.innerText){
+            case 'x':
+                calculation.operator = "x";
+                break;
+            case "+":
+                calculation.operator = "+";
+                break;
+            case "/":
+                calculation.operator = "/";
+                break;
+            case "-":
+                calculation.operator = "-";
+                break;
+        }
+        calculation.num2 = 1;
+        deletion = 1;
+    });
+});
+
+buttons.equal.addEventListener("click", () =>{
+    display.input.innerText = operate(calculation.num1, calculation.operator, calculation.num2);
+})
